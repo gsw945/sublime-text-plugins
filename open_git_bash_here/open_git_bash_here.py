449347583c50ba541c7f61e6a87bdb81db04919a
玖亖伍 <gsw945@foxmail.com>
2019-03-30 09:22:27 +0800

    Update open_git_bash_here.py
    解决git-bash.exe所在目录未添加到环境变量时报错的问题

--- open_git_bash_here/open_git_bash_here.py
+++ open_git_bash_here/open_git_bash_here.py
@@ -11,7 +11,11 @@ class OpenGitBashHereCommand(sublime_plugin.TextCommand):
         dir_name = os.path.dirname(os.path.abspath(file_path))
         env = os.environ.copy()
         args = [self.get_git_path()]
-        subprocess.Popen(args, cwd=dir_name, env=env, shell=True)
+        if args[0] is None:
+            err_msg = '未能找到 git-bash.exe，请将其所在文件夹添加到环境变量PATH中'
+            sublime.message_dialog(err_msg)
+        else:
+            subprocess.Popen(args, cwd=dir_name, env=env, shell=True)
 
     def get_git_path(self):
         '''获取Git-Bash绝对路径'''
@@ -20,6 +24,12 @@ class OpenGitBashHereCommand(sublime_plugin.TextCommand):
             if os.path.exists(os.path.join(item, 'git-bash.exe')):
                 git_path = os.path.join(item, 'git-bash.exe')
                 break
+        if git_path is None:
+            git_exe = subprocess.check_output(['where', 'git.exe'])
+            git_exe = git_exe.decode('utf-8', 'ignore').strip()
+            git_base_dir = os.path.dirname(os.path.dirname(git_exe))
+            if os.path.exists(os.path.join(git_base_dir, 'git-bash.exe')):
+                git_path = os.path.join(git_base_dir, 'git-bash.exe')
         return git_path
         
     def is_visible(self):
